cdaba8faaf626a625a4b5ade69f278cbaea72a3b
RexLin <roadofskyluis@gmail.com>
2017-12-26 13:30:43 +0000

    add admin

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -5,6 +5,7 @@ before_action :require_user , except: [:index, :show]
 before_action :require_same_user , only: [:edit, :update,:destroy]
 
 
+
  def index
   
   @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -59,7 +60,7 @@ before_action :require_same_user , only: [:edit, :update,:destroy]
   end
   
   def require_same_user
-    if current_user!=@article.user
+    if current_user!=@article.user and !current_user.admin?
     flash[:danger]="You can only edit your own article"
     redirect_to root_path
     end  
